bb9d715ea0250cfe6c7f4c7974d4d3fbbabac7cb
kwon-o <koj1228853@gmail.com>
2021-03-04 22:46:24 +0900

    2021/03/04

--- auto_Investment.py
+++ auto_Investment.py
@@ -1,25 +1,46 @@
-import urllib.request
+import requests
 import json
 import pprint
+import urllib.request
 
-obj = { 'APIPassword': '795100' }
-json_data = json.dumps(obj).encode('utf8')
 
-url = 'http://localhost:18080/kabusapi/token'
-req = urllib.request.Request(url, json_data, method='POST')
-req.add_header('Content-Type', 'application/json')
+def get_token():
+    with open('auth.json', 'r') as f:
+        auth = json.load(f)
 
-try:
-    with urllib.request.urlopen(req) as res:
-        print(res.status, res.reason)
-        for header in res.getheaders():
-            print(header)
-        print()
-        content = json.loads(res.read())
+    headers = {'content-type': 'application/json'}
+    json_data = json.dumps(
+        {'APIPassword': auth['APIPassword'], }
+    ).encode('utf8')
+    url = 'http://localhost:18080/kabusapi/token'
+
+    response = requests.post(url, data=json_data, headers=headers)
+
+    return json.loads(response.text)['Token']
+
+
+def get_info():
+    url = 'http://localhost:18080/kabusapi/board/1570@1'
+    req = urllib.request.Request(url, method='GET')
+    req.add_header('Content-Type', 'application/json')
+    req.add_header('X-API-KEY', get_token())
+
+    try:
+        with urllib.request.urlopen(req) as res:
+            print(res.status, res.reason)
+            for header in res.getheaders():
+                print(header)
+            print()
+            content = json.loads(res.read())
+            pprint.pprint(content)
+    except urllib.error.HTTPError as e:
+        print(e)
+        content = json.loads(e.read())
         pprint.pprint(content)
-except urllib.error.HTTPError as e:
-    print(e)
-    content = json.loads(e.read())
-    pprint.pprint(content)
-except Exception as e:
-    print(e)
+    except Exception as e:
+        print(e)
+
+
+get_info()
+
+
---
+++ get_TimeSeriesData.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import requests
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+ETF_link = []
+
+
+def get_link(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    hotKeys = soup.find("table", class_="table1")
+    hotKeys = hotKeys.find_all("a")
+    for i in hotKeys:
+        if i['href'].split('/')[0] == "http:":
+            ETF_link.append(i['href'])
+
+url = "https://s20.si0.kabu.co.jp/members/"
+driver = webdriver.Chrome(executable_path='chromedriver')
+driver.get(url)
+driver.find_element_by_name('SsLogonUser').send_keys('01401160')
+driver.find_element_by_name('SsLogonPassword').send_keys('795100')
+driver.find_element_by_id('image1').click()
+driver.find_element_by_name('nav_g_02').click()
+driver.find_element_by_link_text('フリーETF (手数料無料ETF)').click()
+get_link(driver.page_source)
+s = requests.Session()
+headers = {'User-Agent' : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36"}
+s.headers.update(headers)
+for cookie in driver.get_cookie():
+    c = {cookie['name'] : cookie['value']}
+    s.cookies.update(c)
+link_list = ["https://s20.si0.kabu.co.jp/ap/PC/Stocks/Stock/Search/ByKeyword?PageNo=2&Keyword=%e3%83%95%e3%83%aa%e3%83%bcETF&SortType.Value=0",
+             "https://s20.si0.kabu.co.jp/ap/PC/Stocks/Stock/Search/ByKeyword?PageNo=3&Keyword=%e3%83%95%e3%83%aa%e3%83%bcETF&SortType.Value=0",
+             "https://s20.si0.kabu.co.jp/ap/PC/Stocks/Stock/Search/ByKeyword?PageNo=4&Keyword=%e3%83%95%e3%83%aa%e3%83%bcETF&SortType.Value=0",
+             "https://s20.si0.kabu.co.jp/ap/PC/Stocks/Stock/Search/ByKeyword?PageNo=5&Keyword=%e3%83%95%e3%83%aa%e3%83%bcETF&SortType.Value=0"]
+for i in link_list:
+    response = s.get(i)
+    get_link(response)
+driver.close()
+
+result = pd.DataFrame(ETF_link, columns=['link'])
+result.to_csv('ETF_link.csv', index=False, encoding='utf-8')
