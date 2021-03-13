58081a1c325e345b2bd8db292d41ebcac2f666ec
kwon-o <koj1228853@gmail.com>
2021-03-13 22:30:25 +0900

    2021/03/13

--- DBUpdater.py
+++ DBUpdater.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import mysql.connector as mydb
 import json
+import calendar
 from datetime import datetime
+from selenium import webdriver
+from bs4 import BeautifulSoup
 
 
 class DBUpdater:
@@ -20,9 +23,6 @@ class DBUpdater:
         self.conn.commit()
         self.codes = dict()
 
-    # def __del__(self):
-    #     self.conn.close()
-
     @staticmethod
     def read_stock_code():
         return pd.read_csv('stock_code.csv', encoding='shift_jis')
@@ -39,12 +39,13 @@ class DBUpdater:
             rs = curs.fetchone()
             today = datetime.today().strftime('%Y-%m-%d')
 
-            if rs[0] == None or rs[0].strtime('%Y-%m-%d') < today:
+            if rs[0] == None or rs[0].strftime('%Y-%m-%d') < today:
                 stockCode = self.read_stock_code()
                 for idx in range(len(stockCode)):
                     code = stockCode.code.values[idx]
                     company = stockCode.company.values[idx]
-                    sql = f"REPLACE INTO company_info (code, company, last_update) VALUES ('{code}', '{company}','{today}') "
+                    sql = f"REPLACE INTO company_info " \
+                          f"(code, company, last_update) VALUES ('{code}', '{company}','{today}') "
                     curs.execute(sql)
                     self.codes[code] = company
                     tmnow = datetime.now().strftime('%Y-%m-%d %H:%M')
@@ -52,18 +53,48 @@ class DBUpdater:
                 self.conn.commit()
                 print('')
 
+    @staticmethod
+    def read_Time_Series_Data(code):
+        try:
+            driver = webdriver.Chrome(executable_path='chromedriver.exe')
+            driver.get("https://kabuoji3.com/stock/" + str(code) + "/")
+            source = driver.page_source
+            soup = BeautifulSoup(source, 'html.parser')
+            table = soup.find_all("table", class_='stock_table stock_data_table')
+            df = pd.read_html(str(table), header=0)
 
-    def read_Time_Series_Data(self, code, company):
-        return None
+            if len(df) == 2:
+                df = pd.concat([df[0], df[1]])
+            else:
+                df = df[0]
+            df = df.drop("終値調整", axis=1)
+            df = df.rename(
+                columns={'日付': 'date', '始値': 'open', '高値': 'high', '安値': 'low', '終値': 'close', '出来高': 'volume'})
+            driver.close()
+            return df
+        except ImportError:
+            return None
 
-    def select_comp_info(self):
-        sql = "SELECT * FROM company_info"
-        df = pd.read_sql(sql, self.conn)
-        for idx in range(len(df)):
-            self.codes[df['code'].values[idx]] = df['company'].values[idx]
-        print(self.codes)
+    def replace_into_db(self, df, code):
+        with self.conn.cursor() as curs:
+            for r in df.itertuples():
+                sql = f"REPLACE INTO daily_price VALUES ('{code}', '{r.date}',{r.open},{r.high}," \
+                      f"{r.low},{r.close},{r.volume})"
+                curs.execute(sql)
+            self.conn.commit()
+
+    def update_daily_price(self):
+        for code in self.codes:
+            df = self.read_Time_Series_Data(code)
+            if df is None:
+                continue
+            self.replace_into_db(df, code)
+
+    def execute_daily(self):
+        self.update_comp_info()
+        self.update_daily_price()
 
 
 if __name__ == '__main__':
     dbu = DBUpdater()
-    dbu.select_comp_info()
+    dbu.execute_daily()
