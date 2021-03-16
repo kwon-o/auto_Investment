1e4e11c2f3fa36cfd8bbd369a35045abf6f89d0b
kwon-o <koj1228853@gmail.com>
2021-03-16 22:32:50 +0900

    remove sendslack

--- test/sendSlack.py
+++
@@ -1,9 +0,0 @@
-import json
-import requests
-from slacker import Slacker
-
-slack_webhook_url = "https://hooks.slack.com/services/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"
-headers = {"Content-type": "application/json"}
-data = {"text": "Hello, World!"}
-res = requests.post(slack_webhook_url, headers=headers, data=json.dumps(data))
-print(res.status_code)
